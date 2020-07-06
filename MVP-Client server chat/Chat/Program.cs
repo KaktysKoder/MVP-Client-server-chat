c0e0f76dd6923eebc5a2afa66db2d5c39cfab928
Rimuru <kaktysKoder@gmail.com>
2020-07-06 09:09:40 +0400

    Реализация серверной части (клиент серверного чата).

--- MVP-Client server chat/Chat/Program.cs
+++ MVP-Client server chat/Chat/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Chat
 {
-    class Program
+    internal sealed class Program
     {
-        static void Main(string[] args)
+        const string IP = "127.0.0.1";
+        const int  PORT = 8080;
+
+        private static void Main()
         {
-            Console.WriteLine("Hello TCP & UDP!");
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
+            var tcpSocet    = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            tcpSocet.Bind(tcpEndPoint);
+
+            tcpSocet.Listen(5);
+
+            while (true)
+            {
+                Socket listener = tcpSocet.Accept();
+                byte[] buffer   = new byte[256];
+
+                var size = 0;
+                var data = new StringBuilder();
+
+                do
+                {
+                    size = listener.Receive(buffer);
+
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+
+                }
+                while (listener.Available > 0);
+
+                Console.WriteLine(data);
+
+                listener.Send(Encoding.UTF8.GetBytes("Success!"));
+
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
         }
     }
 }
